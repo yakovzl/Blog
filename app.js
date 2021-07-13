a95deec26421fdf812ef54b0ec6633dad08b7f10
yakovzl <heimzl1996@gmail.com>
2021-07-13 12:39:07 +0300

    ading procfile

--- app.js
+++ app.js
@@ -74,7 +74,5 @@ app.get("/posts/:postId",function (req,res) {
 });
 
 
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
